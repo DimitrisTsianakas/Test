6c56e3118bf3dd43e781599b3b54515047ffd2e9
DimitrisTsianakas <101189667+DimitrisTsianakas@users.noreply.github.com>
2022-03-18 14:21:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,34 +2,36 @@ var options = document.getElementById('options');
 var add_fields = document.getElementById('add_fields');
 var remove_fields = document.getElementById('remove_fields');
 var i = 2;
- function myFunction() {
+var k = 1;
 
-  var newField = document.createElement('input');
-  newField.setAttribute('type','text');
-  newField.setAttribute('name','options[]');
-  newField.setAttribute('class','options');
-  newField.setAttribute('placeholder','Another Field');
-  newField.setAttribute('id',i);
-  options.appendChild(newField);
-        
+function myFunction() {
+    k++;
+    var e = document.getElementById("selOpts");
+    var userSelection = e.value;
+    var row = document.getElementById("display");
+    userSelection = row.insertCell(-1);
+    userSelection.innerHTML = e.value;
+    var newField = document.createElement('input');
+    newField.setAttribute('type', 'text');
+    newField.setAttribute('name', 'options[]');
+    newField.setAttribute('class', 'options');
+    newField.setAttribute('placeholder', e.value);
+    newField.setAttribute('id', 'f' + k);
+    options.appendChild(newField);
 
-  var row = document.getElementById("display");
-  var x = row.insertCell(-1);
-  
-  x.innerHTML = "New cell";
-  i++;
+    i++;
+
 }
 
+var input_tags = options.getElementsByTagName('input');
+remove_fields.onclick = function () {
 
-
-remove_fields.onclick = function() {
-  var input_tags = options.getElementsByTagName('input');
-  if(input_tags.length > 2) {
-    options.removeChild(input_tags[(input_tags.length) - 1]);
-    var row = document.getElementById("display");
-    row.deleteCell(-1);
-     i--;
-  }
+    if (input_tags.length > 2) {
+        options.removeChild(input_tags[(input_tags.length) - 1]);
+        var row = document.getElementById("display");
+        row.deleteCell(-1);
+        i--;
+    }
 }
 
 
@@ -43,33 +45,25 @@ var row = 1;
 function displayDetails() {
     var fName = document.getElementById("fName").value;
     var lastName = document.getElementById("lastName").value;
-
-    
-   
-   
-  
-    
-    if (!fName || !lastName ) {
+    var val = document.getElementById("f" + k).value;
+    if (!fName || !lastName) {
         alert("Συμπληρώστε όλα τα πεδία!");
         return;
     }
-    
-var display = document.getElementById("displays");
-var newRow = display.insertRow(row);
-var cell1 = newRow.insertCell(0);
-var cell2 = newRow.insertCell(1);
-for (let j = 2; j<i; j++) {
-    var cell = newRow.insertCell(j);
-    
+    var display = document.getElementById("displays");
+    var newRow = display.insertRow(row);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    for (let j = 2; j < i; j++) {
+        var cell = newRow.insertCell(j);
+
     }
-cell1.innerHTML = fName;
-cell2.innerHTML = lastName;
+
+    cell1.innerHTML = fName;
+    cell2.innerHTML = lastName;
+    cell.innerHTML = val;
 
 
 
-row++;
+    row++;
 }
-
-
-
-
